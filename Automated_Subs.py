a42e51a4764f618fa33fa030f20cbbf71811bfee
madchun <sccheungad@gmail.com>
2020-02-20 14:13:17 +0800

    add the working steps

---
+++ Automated_Subs.py
@@ -0,0 +1,26 @@
+'''
+WHY: Subscribe Yellow YouTubers more easily
+HOW: Run Python script to automate this process
+WHAT: Go Yellow YouTubers account page and Hit the Subscribe button
+'''
+
+class Subs_Yellow_YouTubers:
+
+    def __init__():
+        pass
+
+    # Step 1: Login to personal YouTube page
+    def get_youtube_client(self):
+        pass
+
+    # Step 2: Get the Yellow YouTubers page url from the Google Sheets
+    def get_youtubers_page_url(arg):
+        pass
+
+    # Step 3: Land on the Yellow YouTubers page
+    def land_page_url():
+        pass
+
+    # Step 4: Subscribe the channel
+    def subs_youtubers():
+        pass
